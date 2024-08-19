d8edf870cc5dc8e1a4ea4299a50a9d7ecf06a9b9
abhinav <contact.abhinavanshul@gmail.com>
2024-08-19 18:41:25 +0530

    fix: arrow icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image"
 
 export default function Home() {
-  return <main className="text-white">WIP</main>
+  return (
+    <>
+      <main className="text-white h-[1000px]">
+        <div className="text-white">hello world</div>
+      </main>
+    </>
+  )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <>
+      <nav className="">
+        <div className="max-w-96 mx-auto py-3 bg-red-500 text-center">
+          <div>SuperUI</div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export { Header }
